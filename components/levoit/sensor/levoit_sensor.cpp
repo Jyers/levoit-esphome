ee69b938cf16051f9c8b31867cbb05bb1dd57057
Jyers <jyers.m@gmail.com>
2026-03-13 16:23:27 -0400

    Switch dry time to hours

--- components/levoit/sensor/levoit_sensor.cpp
+++ components/levoit/sensor/levoit_sensor.cpp
@@ -64,7 +64,7 @@ namespace esphome
           break;
         case SensorType::DRY_TIME_REMAINING:
           this->set_device_class("duration");
-          this->set_unit_of_measurement("s");
+          this->set_unit_of_measurement("h");
           this->set_icon("mdi:timer-sand");
           this->set_accuracy_decimals(0);
           break;
